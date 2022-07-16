66d7142634ec22491d9971315c9da33ab9aa58e7
Neander Menezes <neander2801@gmail.com>
2022-07-16 17:25:05 -0300

    feat(ProductNotFoundException): ProductNotFoundException for service errors

---
+++ src/main/java/com/autoflex/exceptions/ProductNotFoundException.java
@@ -0,0 +1,7 @@
+package com.autoflex.exceptions;
+
+public class ProductNotFoundException extends RuntimeException {
+  public ProductNotFoundException(String message) {
+    super(message);
+  }
+}
