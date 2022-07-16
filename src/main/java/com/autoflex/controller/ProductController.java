d34d004abdcb24cf5ad6be106cb50a5124e2691c
Neander Menezes <neander2801@gmail.com>
2022-07-16 17:24:15 -0300

    feat(ProductController): adds CRUD routes to Product, /product

---
+++ src/main/java/com/autoflex/controller/ProductController.java
@@ -0,0 +1,58 @@
+package com.autoflex.controller;
+
+import com.autoflex.dto.ProductDto;
+import com.autoflex.exceptions.ProductNotFoundException;
+import com.autoflex.model.Product;
+import com.autoflex.service.ProductService;
+import com.autoflex.service.ProductServiceImpl;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.validation.Valid;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@ApplicationScoped
+@Path("/product")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class ProductController {
+
+  private final ProductService productService;
+
+  @Inject
+  public ProductController(ProductService productService) {
+    this.productService = productService;
+  }
+
+  @GET
+  public List<Product> getProducts() {
+    return productService.getAllProducts();
+  }
+
+  @GET
+  @Path("/{id}")
+  public Product getProduct(@PathParam("id") Long id) throws ProductNotFoundException {
+    return productService.getProductById(id);
+  }
+
+  @POST
+  public Product createProduct(@Valid ProductDto productDto) {
+    return productService.saveProduct(productDto.toProduct());
+  }
+
+  @PUT
+  @Path("/{id}")
+  public Product updateProduct(@PathParam("id") Long id, @Valid ProductDto productDto) throws ProductNotFoundException {
+    return productService.updateProduct(id, productDto.toProduct());
+  }
+
+  @DELETE
+  @Path("/{id}")
+  public Response deleteProduct(@PathParam("id") Long id) throws ProductNotFoundException {
+    productService.deleteProduct(id);
+    return Response.status(Response.Status.NO_CONTENT).build();
+  }
+}
