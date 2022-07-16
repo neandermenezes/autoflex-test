d946d52d9c525b7860b46cfa365f1a92958d002a
Neander Menezes <neander2801@gmail.com>
2022-07-16 17:25:41 -0300

    feat(ProductService): created ProductService interface with crud methods

---
+++ src/main/java/com/autoflex/service/ProductService.java
@@ -0,0 +1,19 @@
+package com.autoflex.service;
+
+import com.autoflex.exceptions.ProductNotFoundException;
+import com.autoflex.model.Product;
+
+import java.util.List;
+
+public interface ProductService {
+
+  Product getProductById(Long id) throws ProductNotFoundException;
+
+  List<Product> getAllProducts();
+
+  Product updateProduct(Long id, Product product) throws ProductNotFoundException;
+
+  Product saveProduct(Product product);
+
+  void deleteProduct(Long id) throws ProductNotFoundException;
+}
