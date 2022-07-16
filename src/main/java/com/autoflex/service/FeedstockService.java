bbac513f8b07333964c48d1b43cc5b416abf4756
Neander Menezes <neander2801@gmail.com>
2022-07-16 17:23:18 -0300

    feat(FeedstockService): adds FeedstockService interface methods

---
+++ src/main/java/com/autoflex/service/FeedstockService.java
@@ -0,0 +1,20 @@
+package com.autoflex.service;
+
+import com.autoflex.exceptions.FeedstockNotFoundException;
+import com.autoflex.model.Feedstock;
+import com.autoflex.model.Product;
+
+import java.util.List;
+
+public interface FeedstockService {
+
+  Feedstock getFeedstockById(Long id) throws FeedstockNotFoundException;
+
+  List<Feedstock> getAllFeedstock();
+
+  Feedstock updateFeedstock(Long id, Feedstock feedstock) throws FeedstockNotFoundException;
+
+  Feedstock saveFeedstock(Feedstock feedstock);
+
+  void deleteFeedstock(Long id) throws FeedstockNotFoundException;
+}
