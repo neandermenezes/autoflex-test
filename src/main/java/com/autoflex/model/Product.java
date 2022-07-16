a936a770a2372ca114b93660b2bec7b3f53653aa
Neander Menezes <neander2801@gmail.com>
2022-07-16 17:20:31 -0300

    setup: add dependencies

--- src/main/java/com/autoflex/model/Product.java
+++ src/main/java/com/autoflex/model/Product.java
@@ -1,30 +1,44 @@
 package com.autoflex.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Entity
 public class Product {
 
+  public Product(String name, Integer valor) {
+    this.name = name;
+    this.valor = valor;
+  }
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Integer id;
+  private Long id;
 
   private String name;
 
   private Integer valor;
 
-  @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+  @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.LAZY)
   @JoinTable(name = "product_feedstock",
     joinColumns = {@JoinColumn(name = "product_id")},
     inverseJoinColumns = {@JoinColumn(name = "feedstock_id")})
   private List<Feedstock> feedstocks;
 
-  public Integer getId() {
+  public Product() {
+
+  }
+
+  public Long getId() {
     return id;
   }
 
-  public void setId(Integer id) {
+  public void setId(Long id) {
     this.id = id;
   }
 
