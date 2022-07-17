d33a65f0c5e70268056d43ebe4a6faec30429d75
Neander Menezes <neander2801@gmail.com>
2022-07-17 12:33:18 -0300

    feat(index): add get endpoints for products and feedstocks

---
+++ autoflex-front/src/service/index.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: `http://localhost:${process.env.REACT_APP_API_PORT || '8080'}`,
+  timeout: 1000,
+});
+
+export const requestData = async (endpoint) => {
+  const { data } = await api.get(endpoint);
+  return data;
+}
+
+export const deleteData = async (id, endpoint) => {
+  await api.delete(`${endpoint}/${id}`);
+}
