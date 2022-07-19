c4ea39f99903a09aede52e317c3e43ae19217f5c
Neander Menezes <neander2801@gmail.com>
2022-07-19 13:29:29 -0300

    feat: add logic to create product attached with its feedstocks

--- autoflex-front/src/components/ProductForm.jsx
+++ autoflex-front/src/components/ProductForm.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import "../styles/ProductForm.css";
 import Context from "../context/Context";
 import FeedstockTable from "./FeedstockTable";
+import { createData } from "../service";
 
 export default function ProductForm() {
   const [name, setName] = useState("");
@@ -14,66 +15,110 @@ export default function ProductForm() {
 
   useEffect(() => {
     setAvailableFeedstocks(feedstocks);
-  })
+  });
 
-  return (
-    isCreating ? (
-      <div className="product-form">
-        <h2>Create Product</h2>
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          id="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <label htmlFor="value">Value</label>
-        <input
-          type="text"
-          id="value"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        />
+  const handleAddFeedstock = (feedstock, isChecked) => {
+    if (!isChecked) {
+      setFeedstocksToAdd(feedstocksToAdd.filter((f) => f.id !== feedstock.id));
+    } else {
+      setFeedstocksToAdd([...feedstocksToAdd, feedstock]);
+    }
+    console.log(feedstock, isChecked);
+  };
+
+  const handleCreateData = async () => {
+    setIsCreating(false);
+
+    const data = {
+      name,
+      value,
+      feedstockIds: feedstocksToAdd.map((f) => f.id),
+    };
+
+    await createData(data, "/product");
+
+    setIsCreating(false);
+    setName("");
+    setValue("");
+    setFeedstocksToAdd([]);
+
+    window.location.reload(false);
+  };
+
+  return isCreating ? (
+    <div className="product-form">
+      <h2 className="product-form__title">Create Product</h2>
+      <label className="product-form__label" htmlFor="name">
+        Name
+      </label>
+      <input
+        className="product-form__input"
+        type="text"
+        id="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <label className="product-form__label" htmlFor="value">
+        Value
+      </label>
+      <input
+        className="product-form__input"
+        type="text"
+        id="value"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+
+      <div className="btns">
         <button
+          className="product-form__btn"
           onClick={() => {
             setIsCreating(false);
             setName("");
             setValue("");
+            setFeedstocksToAdd([]);
           }}
         >
           Cancel
         </button>
         <button
+          className="product-form__btn"
+          disabled={!name || !value || !feedstocksToAdd.length}
           onClick={() => {
             setIsCreating(false);
             setName("");
             setValue("");
+            handleCreateData();
           }}
         >
           Create
         </button>
-        <div className="product-form__feedstock">
-          <h3>Feedstocks</h3>
-          { availableFeedstocks && availableFeedstocks.map((feedstock) => (
-            <div key={feedstock.id}>
-              <p>{feedstock.id}</p>
-              <p>{feedstock.name}</p>
-              <p>{feedstock.quantity}</p>
-              <button
-                onClick={() => {
-                  setFeedstocksToAdd([...feedstocksToAdd, feedstock]);
-                  setAvailableFeedstocks((feedstock.id) => )
+      </div>
+
+      <div className="product-form__feedstock">
+        <h3 className="product-form__title">
+          Necessary feedstocks to produce:{" "}
+        </h3>
+        {availableFeedstocks &&
+          availableFeedstocks.map((feedstock) => (
+            <div className="feedstock" key={feedstock.id}>
+              <input
+                onClick={({ target }) =>
+                  handleAddFeedstock(feedstock, target.checked)
                 }
-                }
-              >
-                Add
-              </button>
+                type="checkbox"
+              />
+              <p className="feedstock__name">{feedstock.name}</p>
             </div>
           ))}
-        </div>
       </div>
-    ) : (
-      <button onClick={ () => setIsCreating(true) } className="product-form__btn--create">Create Product</button>
-    )
+    </div>
+  ) : (
+    <button
+      onClick={() => setIsCreating(true)}
+      className="product-form__btn--create"
+    >
+      Create Product
+    </button>
   );
 }
