989a979daefb3f61a770284219313044872c01cc
Neander Menezes <neander2801@gmail.com>
2022-07-19 13:27:29 -0300

    setup: add Page components to App

--- autoflex-front/src/App.jsx
+++ autoflex-front/src/App.jsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import Products from "./pages/Products";
-import './styles/App.css';
+import Feedstocks from "./pages/Feedstocks";
+import FeedstockProducts from "./pages/FeedstockProducts";
+import "./styles/App.css";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Products />} />
+        <Route path="/feedstock" element={<Feedstocks />} />
+        <Route path="/feedstock/products" element={<FeedstockProducts />} />
       </Routes>
     </div>
   );
