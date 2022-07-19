1193e51000fa83d0386adbda6d1d43d5d9bfa09f
Neander Menezes <neander2801@gmail.com>
2022-07-19 13:29:00 -0300

    feat: add buttons to navigate through application

--- autoflex-front/src/components/Header.jsx
+++ autoflex-front/src/components/Header.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import '../styles/Header.css'
+import React from "react";
+import "../styles/Header.css";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export default function Header() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const navigationHelper = {
+    "/": "feedstock",
+    "/feedstock": "/",
+    "/feedstock/products": "/",
+  };
+
   return (
     <header className="header">
+      <button className="header__btn" onClick={() => navigate(navigationHelper[location.pathname])}>
+        {location.pathname === "/" ? "Go to Feedstocks" : "Go to Products"}
+      </button>
+      <h1>
       Autoflex Store
+      </h1>
+      { location.pathname !== "/feedstock/products" && (
+        <button className="header__btn" onClick={() => navigate("/feedstock/products")}>
+          Go to Feedstock Products
+        </button>
+      )}
     </header>
-  )
+  );
 }
